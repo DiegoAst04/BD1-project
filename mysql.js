3c3cd37194e0df52e51b73977cf0f0c5a9623bd1
ValenSaZu <camila.salazar@ucsp.edu.pe>
2024-06-14 11:19:15 -0500

    Register sql

--- mysql.js
+++ mysql.js
@@ -26,9 +26,15 @@ app.use(express.urlencoded({ extended: false }));
 
 // Ruta para el login
 app.get("/", (req, res) => {
+    res.render("login");
+});
+app.post("/login", (req, res) => {
+    const infoLogin = req.body;
+    console.log(infoLogin);
+})
+app.get("/register", (req, res) => {
     res.render("register");
 });
-
 // Validar datos de registro
 app.post("/register", (req, res) => {
     const infoRegister = req.body;
@@ -55,13 +61,50 @@ app.post("/register", (req, res) => {
             res.status(400).send("El usuario ya tiene una cuenta");
         } else {
             let salario;
-            if (puesto === 'p1') salario = 75;
-            if (puesto === 'p2') salario = 60;
-            if (puesto === 'p3') salario = 100;
-            if (puesto === 'p4') salario = 65;
 
-            const registrar = "INSERT INTO empleado (DNI, nombre, apellido, puesto, teléfonos, salario, contrasenia) VALUES (?, ?, ?, ?, ?, ?, ?)";
-            const values = [dni, nombre, apellido, puesto, telefono, salario, contrasenia];
+            switch (puesto) {
+                case 'p1':
+                    salario = 75;
+                    break;
+                case 'p2':
+                    salario = 60;
+                    break;
+                case 'p3':
+                    salario = 100;
+                    break;
+                case 'p4':
+                    salario = 65;
+                    break;
+                default:
+                    // En caso de que el puesto no coincida con ninguno de los especificados
+                    salario = 0; // Puedes manejarlo de acuerdo a tus requisitos
+                    break;
+            }
+
+            const Query_ID_Sucursal = "SELECT ID from Sucursal WHERE direccion = ?";
+            var ID_Sucursal_;
+            connection.query(Query_ID_Sucursal,[adresses], (err) => {
+                if (err) {
+                    console.error('Error: ', err.message);
+                    res.status(500).send("No existe una sucursal con este dirección");
+                    return;
+                }
+                ID_Sucursal_ = Query_ID_Sucursal;
+            });
+            const ID_Caja = null;
+            if(puesto=='Cajero'){
+                const Query_ID_Caja = "SELECT ID from Caja WHERE ID_Sucursal = ?";
+                connection.query(Query_ID_Caja,[ID_Sucursal_], (err) => {
+                if (err) {
+                    console.error('Error: ', err.message);
+                    return;
+                }
+                ID_Caja = Query_ID_Caja;
+            });
+            }
+            const ID_Horarios = null;
+            const registrar = "INSERT INTO empleado (DNI, nombre, apellido, puesto, contrasenia, salario, ID_Sucursal, ID_Caja, ID_Horarios) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            const values = [dni, nombre, apellido, puesto, contrasenia, salario, contrasenia, ID_Sucursal_, ID_Caja, ID_Horarios];
 
             connection.query(registrar, values, (err) => {
                 if (err) {
